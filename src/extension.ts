2963bc3aa74099b00c008e74370753a2ad39fbf3
Mims H. Wright <mims@mimswright.com>
2026-04-10 23:26:07 +0200

    feat: initialize vscode extension project structure

---
+++ src/extension.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode';
+
+export async function activate(context: vscode.ExtensionContext) {
+  console.log('Auto Project Theme activated');
+
+  // TODO: Initialize managers and register commands
+}
+
+export function deactivate() {}
